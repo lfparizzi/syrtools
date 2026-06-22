6cdc4c5898b895c583efee7914663d8afe9b9a4a
lfparizzi <66044243+lfparizzi@users.noreply.github.com>
2026-06-22 11:38:39 -0300

    Update installation messages in installer.sh

--- installer.sh
+++ installer.sh
@@ -15,20 +15,24 @@ sudo apt-get install libsasl2-dev python2-dev libldap2-dev libssl-dev -y
 pip3 install pwn==1.0 --break-system-packages
 pip3 install python-ldap==3.4.0 --break-system-packages
 echo ""
-echo "FIM DA INSTALAÇÃO DO ADENUM"
+echo "@@ FIM DA INSTALAÇÃO DO ADENUM @@"
 
 
 # Instala o Feroxbuster
 sudo apt install feroxbuster -y
 echo ""
-echo "FIM DA INSTALAÇÃO DO FEROXBUSTER"
+echo "@@ FIM DA INSTALAÇÃO DO FEROXBUSTER @@"
 
+# Instala o Tree
+sudo apt install tree -y
+echo ""
+echo "@@ FIM DA INSTALAÇÃO DO TREE @@"
 
 # Instala o GAU
 tar xvf programas/gau_2.2.4_linux_amd64.tar.gz -C programas/
 mv programas/gau /usr/bin/gau
 echo ""
-echo "FIM DA INSTALAÇÃO DO GAU"
+echo "@@ FIM DA INSTALAÇÃO DO GAU @@"
 
 #fim
 echo "░▀█▀░█▀█░█▀▀░▀█▀░█▀█░█░░░█░░░█▀▀░█▀▄░░░█▀▄░█▀█░█▀█░█▀▀
